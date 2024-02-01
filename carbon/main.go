ac294e7a090b9d17060010e92ad5115891588bbf
dulei <dulei@rcrai.com>
2024-02-01 15:45:25 +0800

    feat: fix carbon using

--- carbon/main.go
+++ carbon/main.go
@@ -16,16 +16,11 @@ func main() {
 	// fmt.Println(c.Now().Season())
 	// fmt.Println("Hello, World!")
 
+	fmt.Println(carbon.Now().Layout(carbon.RFC3339Layout))
+	fmt.Println(carbon.Now().Format(carbon.RFC3339Format))
+
+	// not recommend
 	fmt.Println(time.Now().Format(carbon.RFC3339MilliLayout))
 	fmt.Println(time.Now().Format(carbon.RFC3339Layout))
 
-	tm, err := time.Parse("2006-01-02T15:04:05.999Z07:00", "2023-12-20T14:10:47+08:00")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(time.Now())
-	fmt.Println(tm)
-	fmt.Println(tm.Local().Format("2006-01-02T15:04:05.000Z07:00"))
-	fmt.Println(time.Now().Format("2006-01-02T15:04:05.000Z07:00"))
-	fmt.Println(tm.Local().Format(time.StampMilli))
 }
